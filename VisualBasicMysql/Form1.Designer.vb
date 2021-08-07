ba3d038df2c59d05df4501cb19efc1786c9f251f
Programador Novato <info@programadornovato.com>
2021-08-07 02:28:22 -0500

    Llenar nuestro DataGridView (tabla) con MySQL (MariaDB) en Visual Basic⛓️ [03] Video: https://youtu.be/hgg44L9In-g

--- VisualBasicMysql/Form1.Designer.vb
+++ VisualBasicMysql/Form1.Designer.vb
@@ -24,6 +24,12 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.btnConectar = New System.Windows.Forms.Button()
         Me.lblResultado = New System.Windows.Forms.Label()
+        Me.dgTrabajadores = New System.Windows.Forms.DataGridView()
+        Me.colId = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colNombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colPuesto = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colEdad = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.dgTrabajadores, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btnConectar
@@ -38,22 +44,55 @@ Partial Class Form1
         'lblResultado
         '
         Me.lblResultado.AutoSize = True
-        Me.lblResultado.Location = New System.Drawing.Point(63, 129)
+        Me.lblResultado.Location = New System.Drawing.Point(63, 377)
         Me.lblResultado.Name = "lblResultado"
         Me.lblResultado.Size = New System.Drawing.Size(333, 32)
         Me.lblResultado.TabIndex = 1
         Me.lblResultado.Text = "Conexion a la base de datos"
+        '
+        'dgTrabajadores
+        '
+        Me.dgTrabajadores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgTrabajadores.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colId, Me.colNombre, Me.colPuesto, Me.colEdad})
+        Me.dgTrabajadores.Location = New System.Drawing.Point(63, 129)
+        Me.dgTrabajadores.Name = "dgTrabajadores"
+        Me.dgTrabajadores.RowTemplate.Height = 25
+        Me.dgTrabajadores.Size = New System.Drawing.Size(385, 213)
+        Me.dgTrabajadores.TabIndex = 2
+        '
+        'colId
+        '
+        Me.colId.HeaderText = "id"
+        Me.colId.Name = "colId"
+        Me.colId.Visible = False
+        '
+        'colNombre
+        '
+        Me.colNombre.HeaderText = "Nombre"
+        Me.colNombre.Name = "colNombre"
+        '
+        'colPuesto
+        '
+        Me.colPuesto.HeaderText = "Puesto"
+        Me.colPuesto.Name = "colPuesto"
+        '
+        'colEdad
+        '
+        Me.colEdad.HeaderText = "Edad"
+        Me.colEdad.Name = "colEdad"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(14.0!, 32.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgTrabajadores)
         Me.Controls.Add(Me.lblResultado)
         Me.Controls.Add(Me.btnConectar)
         Me.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.dgTrabajadores, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -61,4 +100,9 @@ Partial Class Form1
 
     Friend WithEvents btnConectar As Button
     Friend WithEvents lblResultado As Label
+    Friend WithEvents dgTrabajadores As DataGridView
+    Friend WithEvents colId As DataGridViewTextBoxColumn
+    Friend WithEvents colNombre As DataGridViewTextBoxColumn
+    Friend WithEvents colPuesto As DataGridViewTextBoxColumn
+    Friend WithEvents colEdad As DataGridViewTextBoxColumn
 End Class
--- VisualBasicMysql/Form1.vb
+++ VisualBasicMysql/Form1.vb
@@ -10,6 +10,20 @@ Public Class Form1
             Dim myCon = New MySqlConnection(cadena)
             myCon.Open()
             lblResultado.Text = "Conexion es exitosa"
+            Dim query = "SELECT id, nombre, puesto, edad FROM trabajadores"
+            Dim comando = New MySqlCommand(query, myCon)
+            Dim reader = comando.ExecuteReader()
+            If reader.HasRows Then
+                While reader.Read()
+                    Dim n = dgTrabajadores.Rows.Add()
+                    dgTrabajadores.Rows(n).Cells(0).Value = reader.GetString(0).ToString
+                    dgTrabajadores.Rows(n).Cells(1).Value = reader.GetString(1).ToString
+                    dgTrabajadores.Rows(n).Cells(2).Value = reader.GetString(2).ToString
+                    dgTrabajadores.Rows(n).Cells(3).Value = reader.GetString(3).ToString
+                End While
+            Else
+                Console.WriteLine("No hay trabajadores")
+            End If
         Catch ex As Exception
             lblResultado.Text = "error de conexion " & ex.ToString
         End Try
