a78b6079d148a84346f522c6b18bd816afafe918
Programador Novato <info@programadornovato.com>
2021-08-08 03:08:30 -0500

    Buscar registros en MySQL (MariaDB) con Visual Basic⛓️ [04] Video: https://youtu.be/zOy73cERme8

--- VisualBasicMysql/Form1.Designer.vb
+++ VisualBasicMysql/Form1.Designer.vb
@@ -22,29 +22,26 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnConectar = New System.Windows.Forms.Button()
         Me.lblResultado = New System.Windows.Forms.Label()
         Me.dgTrabajadores = New System.Windows.Forms.DataGridView()
         Me.colId = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.colNombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.colPuesto = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.colEdad = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtBusNombre = New System.Windows.Forms.TextBox()
+        Me.txtBusPuesto = New System.Windows.Forms.TextBox()
+        Me.txtBusEdad = New System.Windows.Forms.TextBox()
+        Me.btnBuscar = New System.Windows.Forms.Button()
         CType(Me.dgTrabajadores, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'btnConectar
-        '
-        Me.btnConectar.Location = New System.Drawing.Point(63, 62)
-        Me.btnConectar.Name = "btnConectar"
-        Me.btnConectar.Size = New System.Drawing.Size(152, 44)
-        Me.btnConectar.TabIndex = 0
-        Me.btnConectar.Text = "Conectar"
-        Me.btnConectar.UseVisualStyleBackColor = True
         '
         'lblResultado
         '
         Me.lblResultado.AutoSize = True
-        Me.lblResultado.Location = New System.Drawing.Point(63, 377)
+        Me.lblResultado.Location = New System.Drawing.Point(30, 377)
         Me.lblResultado.Name = "lblResultado"
         Me.lblResultado.Size = New System.Drawing.Size(333, 32)
         Me.lblResultado.TabIndex = 1
@@ -54,10 +51,10 @@ Partial Class Form1
         '
         Me.dgTrabajadores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgTrabajadores.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colId, Me.colNombre, Me.colPuesto, Me.colEdad})
-        Me.dgTrabajadores.Location = New System.Drawing.Point(63, 129)
+        Me.dgTrabajadores.Location = New System.Drawing.Point(30, 129)
         Me.dgTrabajadores.Name = "dgTrabajadores"
         Me.dgTrabajadores.RowTemplate.Height = 25
-        Me.dgTrabajadores.Size = New System.Drawing.Size(385, 213)
+        Me.dgTrabajadores.Size = New System.Drawing.Size(556, 245)
         Me.dgTrabajadores.TabIndex = 2
         '
         'colId
@@ -70,25 +67,90 @@ Partial Class Form1
         '
         Me.colNombre.HeaderText = "Nombre"
         Me.colNombre.Name = "colNombre"
+        Me.colNombre.Width = 200
         '
         'colPuesto
         '
         Me.colPuesto.HeaderText = "Puesto"
         Me.colPuesto.Name = "colPuesto"
+        Me.colPuesto.Width = 200
         '
         'colEdad
         '
         Me.colEdad.HeaderText = "Edad"
         Me.colEdad.Name = "colEdad"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 32)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(229, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 32)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Puesto"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(434, 24)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 32)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Edad"
+        '
+        'txtBusNombre
+        '
+        Me.txtBusNombre.Location = New System.Drawing.Point(30, 72)
+        Me.txtBusNombre.Name = "txtBusNombre"
+        Me.txtBusNombre.Size = New System.Drawing.Size(152, 39)
+        Me.txtBusNombre.TabIndex = 5
+        '
+        'txtBusPuesto
+        '
+        Me.txtBusPuesto.Location = New System.Drawing.Point(229, 72)
+        Me.txtBusPuesto.Name = "txtBusPuesto"
+        Me.txtBusPuesto.Size = New System.Drawing.Size(152, 39)
+        Me.txtBusPuesto.TabIndex = 5
+        '
+        'txtBusEdad
+        '
+        Me.txtBusEdad.Location = New System.Drawing.Point(434, 72)
+        Me.txtBusEdad.Name = "txtBusEdad"
+        Me.txtBusEdad.Size = New System.Drawing.Size(152, 39)
+        Me.txtBusEdad.TabIndex = 5
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(648, 72)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(120, 39)
+        Me.btnBuscar.TabIndex = 6
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(14.0!, 32.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.txtBusEdad)
+        Me.Controls.Add(Me.txtBusPuesto)
+        Me.Controls.Add(Me.txtBusNombre)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.dgTrabajadores)
         Me.Controls.Add(Me.lblResultado)
-        Me.Controls.Add(Me.btnConectar)
         Me.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -97,12 +159,17 @@ Partial Class Form1
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents btnConectar As Button
     Friend WithEvents lblResultado As Label
     Friend WithEvents dgTrabajadores As DataGridView
     Friend WithEvents colId As DataGridViewTextBoxColumn
     Friend WithEvents colNombre As DataGridViewTextBoxColumn
     Friend WithEvents colPuesto As DataGridViewTextBoxColumn
     Friend WithEvents colEdad As DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtBusNombre As TextBox
+    Friend WithEvents txtBusPuesto As TextBox
+    Friend WithEvents txtBusEdad As TextBox
+    Friend WithEvents btnBuscar As Button
 End Class
--- VisualBasicMysql/Form1.vb
+++ VisualBasicMysql/Form1.vb
@@ -1,13 +1,14 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Form1
-    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+    Private myCon As MySqlConnection
+    Private Sub llenarTabla()
         Try
             Dim server = "localhost"
             Dim usuario = "root"
             Dim pass = "123456"
             Dim db = "empleados"
             Dim cadena = "server=" & server & ";Uid=" & usuario & ";pwd=" & pass & ";database=" & db & ";SslMode=none;"
-            Dim myCon = New MySqlConnection(cadena)
+            myCon = New MySqlConnection(cadena)
             myCon.Open()
             lblResultado.Text = "Conexion es exitosa"
             Dim query = "SELECT id, nombre, puesto, edad FROM trabajadores"
@@ -22,11 +23,51 @@ Public Class Form1
                     dgTrabajadores.Rows(n).Cells(3).Value = reader.GetString(3).ToString
                 End While
             Else
-                Console.WriteLine("No hay trabajadores")
+                lblResultado.Text = "No hay trabajadores"
             End If
+            reader.Close()
         Catch ex As Exception
             lblResultado.Text = "error de conexion " & ex.ToString
         End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        llenarTabla()
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim where = " where 1=1 "
+        If txtBusNombre.Text <> "" Then
+            where = where & " and nombre like '%" & txtBusNombre.Text & "%' "
+        End If
+        If txtBusPuesto.Text <> "" Then
+            where = where & " and puesto like '%" & txtBusPuesto.Text & "%' "
+        End If
+        If txtBusEdad.Text <> "" Then
+            where = where & " and edad='" & txtBusEdad.Text & "' "
+        End If
+        Try
+            Dim query = "SELECT id, nombre, puesto, edad FROM trabajadores " & where
+            Dim comando = New MySqlCommand(query, myCon)
+            Dim reader = comando.ExecuteReader()
+            dgTrabajadores.Rows.Clear()
+            dgTrabajadores.Refresh()
+            If reader.HasRows Then
+                While reader.Read()
+                    Dim n = dgTrabajadores.Rows.Add()
+                    dgTrabajadores.Rows(n).Cells(0).Value = reader.GetString(0).ToString
+                    dgTrabajadores.Rows(n).Cells(1).Value = reader.GetString(1).ToString
+                    dgTrabajadores.Rows(n).Cells(2).Value = reader.GetString(2).ToString
+                    dgTrabajadores.Rows(n).Cells(3).Value = reader.GetString(3).ToString
+                End While
+            Else
+                Console.WriteLine("No hay trabajadores")
+            End If
+            reader.Close()
+        Catch ex As Exception
+            lblResultado.Text = ex.ToString
+        End Try
+
 
     End Sub
 End Class
